b6dbc14fb2e385a95c67364da51aeab058763d3c
unknown <majdlila777@gmail.com>
2026-03-22 10:08:12 +0300

    change database type

--- prisma/migrations/20260321083029_add_subtasks_and_priority/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateEnum
-CREATE TYPE "TaskPriority" AS ENUM ('LOW', 'MEDIUM', 'HIGH', 'CRITICAL');
-
--- AlterTable
-ALTER TABLE "tasks" ADD COLUMN     "parent_id" UUID,
-ADD COLUMN     "priority" "TaskPriority" NOT NULL DEFAULT 'MEDIUM';
-
--- AddForeignKey
-ALTER TABLE "tasks" ADD CONSTRAINT "tasks_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
